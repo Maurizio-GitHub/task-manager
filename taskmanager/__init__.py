8b3a0ce6b4c9e530d7211de971ee850a288083f2
Maurizio <maurizio.loffredo@outlook.com>
2023-02-01 21:50:08 +0000

    Feat: added requirements.txt, Procfile and code to __init__.py

--- taskmanager/__init__.py
+++ taskmanager/__init__.py
@@ -18,7 +18,15 @@ app = Flask(__name__)
 # Then, we need to specify two app configuration variables.
 # They both come from our environment variables:
 app.config["SECRET_KEY"] = os.environ.get("SECRET_KEY")
-app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+if os.environ.get("DEVELOPMENT") == "True":
+    app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("DB_URL")
+else:
+    # To ensure that SQLAlchemy can also read our external database,
+    # its URL needs to start with "postgresql://":
+    uri = os.environ.get("DATABASE_URL")
+    if uri.startswith("postgres://"):
+        uri = uri.replace("postgres://", "postgresql://", 1)
+        app.config["SQLALCHEMY_DATABASE_URI"] = uri
 
 # We also need to create an instance of the imported SQLAlchemy() class,
 # assigned to a variable of 'db' and set to the instance of our Flask 'app':
